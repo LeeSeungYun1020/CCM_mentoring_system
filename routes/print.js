f4d28327c6cd46542cc3f945901545bfe7fb99f9
shimjaeyeong <jysim0129@naver.com>
2020-11-10 15:35:59 +0900

    Modified edit.html => Add chips function

---
+++ routes/print.js
@@ -0,0 +1,19 @@
+// const express = require('express')
+// const router = express.Router()
+// const mysql = require('../lib/mysql.js')
+//
+// module.exports = function (passport) {
+//     router.route('/')
+//         .get(function (req, res) {
+//             if (req.user) {
+//                 res.redirect("/users/")
+//             } else
+//                 res.render("edit.html")
+//         })
+//         .post(((req, res) => {
+//             res.send(req.user)
+//         }))
+//
+//
+//
+// }
